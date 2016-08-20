697dc47775578df39d1918b2b847f82f6c80230a
DESKTOP-OAS0JGN\Seabass <cookieboy21@gmail.com>
2016-08-20 10:33:27 -0700

    Compare, CompareTo, Seabass

--- Sebastian/ConsoleApplication_Strings/ConsoleApplication_Strings/Program.cs
+++ Sebastian/ConsoleApplication_Strings/ConsoleApplication_Strings/Program.cs
@@ -10,7 +10,21 @@ namespace ConsoleApplication_Strings
     {
         static void Main(string[] args)
         {
+            string str0 = "Word A";
+            string str1 = "Word B";
             Console.WriteLine("This is a console application meant for exploring the power of 'strings'.");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Comparing " + str0 + " and " + str1);
+
+            int value0 = string.Compare(str0, str1);
+            Console.WriteLine(value0);
+            value0 = str0.CompareTo(str1);
+            Console.WriteLine(value0);
+            value0 = str1.CompareTo(str0);
+            Console.WriteLine(value0);
+
+            Console.WriteLine("End program");
             Console.ReadKey();
         }
     }
